d0be726bb2c229acb292d2604f111a97bf4199c5
Es-sehayli Ayoub <ayoub1essehayli@gmail.com>
2025-10-01 10:03:54 +0100

    Test Driver for Integration Testing

---
+++ test_driver/app.dart
@@ -0,0 +1,16 @@
+import 'dart:io' show File;
+
+import 'package:integration_test/integration_test_driver_extended.dart';
+
+Future<void> main() async {
+  await integrationDriver(
+    onScreenshot:
+        (String fileName, List<int> image, [Map<String, Object?>? args]) async {
+          final file = await File(
+            'screenshots/$fileName.png',
+          ).create(recursive: true);
+          file.writeAsBytesSync(image);
+          return true;
+        },
+  );
+}
---
+++ test_driver/app_test.dart
@@ -0,0 +1,23 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_start/main.dart';
+import 'package:integration_test/integration_test.dart';
+
+void main() {
+  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets('tap on the floating action button, verify counter', (
+      tester,
+    ) async {
+      // Load app widget.
+      await tester.pumpWidget(const MyApp());
+
+      // Screenshot step
+      await binding.convertFlutterSurfaceToImage();
+      await tester.pumpAndSettle();
+      await binding.takeScreenshot('test-homepage');
+
+      expect(true, true);
+    });
+  });
+}
